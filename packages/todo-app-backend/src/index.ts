e6fc7aff37d816275174b79d73101e5b0feb231e
Alex González <alejandrogsa@gmail.com>
2022-04-06 05:51:26 +0200

    feat(api): deploy configuration

--- packages/todo-app-backend/src/index.ts
+++ packages/todo-app-backend/src/index.ts
@@ -4,15 +4,14 @@ import { MongoClientInstance } from './database/mongo-client-instance';
 
 dotenv.config();
 
-const port = process.env.APP_PORT ?? 4000;
+const port = process.env.PORT ?? 4000;
 const mongoUri = process.env.MONGO_URI ?? '';
 const dbName = process.env.DDBB_NAME ?? '';
 
 /**
  * Starts the database pool connection, saving it into the app
  */
-MongoClientInstance.getInstance(mongoUri, dbName)
-    .then(instance => {
-        app.locals.db = instance?.db;
-        app.listen(port, () => console.log(`⚡️[server]: Server listening in port ${port}`));
-    });
+MongoClientInstance.getInstance(mongoUri, dbName).then((instance) => {
+  app.locals.db = instance?.db;
+  app.listen(port, () => console.log(`⚡️[server]: Server listening in port ${port}`));
+});
